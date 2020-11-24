91360a6c8ba45332ae54b32efa645b42c787cfde
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-11-24 18:53:11 +0100

    new best model

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,2 @@
-python train.py --seed 42 --txt_file ./assets/book_NL_tolstoy_anna_karenina.txt --batch_size 32 --train_steps=250 --seq_length 30 --print_every 1000 --sample_every 1000 --learning_rate 2e-3 --lstm_num_layers 2 --dropout_keep_prob 0.1 --learning_rate_decay=0.5
+# Command run for generating plots section 2.1(a)
+python train.py --seed 52 --txt_file ./assets/book_NL_tolstoy_anna_karenina.txt --batch_size 64 --train_steps=19950 --seq_length 30 --print_every 1000 --sample_every 5000 --learning_rate 2e-3 --lstm_num_layers 2 --dropout_keep_prob 0.1 --learning_rate_decay=0.5
